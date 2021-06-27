a1c55c3b29e99009700d708307c382431e7f4aa5
Eli Alejandro <iscelialejandro@gmail.com>
2021-06-27 12:48:31 -0500

    Primer commit

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { environment } from '../../environments/environment';
+
+const CLIENT_ID = environment.clientId;
+const CLIENT_SECRET = environment.clientSecret;
+const URL = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  login(username: string, password: string) {
+    const data = {
+      grant_type: 'password',
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      username,
+      password,
+      scope: ''
+    };
+
+    this.http.post(`${ URL }/oauth/token`, data)
+      .subscribe((response: any) => {
+        console.log(response);
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('resfresh_token', response.resfresh_token);
+      });
+  }
+
+  token() {
+    return localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.clear();
+  }
+
+}
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthenticationService
+  ) {}
+
+  tabCamara() {
+    console.log('ir a tab2');
+    this.navCtrl.navigateForward('/tabs/tab2');
+  }
+
+  ngOnInit() {
+    this.authService.login('dixie.buckridge@example.com', 'password');
+  }
+
+}
